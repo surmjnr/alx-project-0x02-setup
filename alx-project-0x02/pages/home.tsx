4d94bacea8b224133fe3b846c5ba92eb24afbac3
Banafo <surmjnr@gmail.com>
2025-06-06 09:32:57 +0000

    fix: update Card component imports and paths

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
-import Card from '../components/common/Card';
+import Card from '@/components/common/Card';
 
 const Home: React.FC = () => {
   return (
